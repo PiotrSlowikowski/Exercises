1845f075acded61ed05bff4fdd6c2fb13314e28f
Piotr Slowikowski <peter.slowikowski@hotmail.com>
2019-10-01 17:32:23 +0200

    5th day

--- src/com/codewars/Main.java
+++ src/com/codewars/Main.java
@@ -8,8 +8,43 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println(morseDecoder(".... . -.--  -.. ..- -.. ."));;
+//        System.out.println(morseDecoder(".... . -.--  -.. ..- -.. ."));;
 
+//        int[] numbers = {2,1,1,1,1,1};
+//        System.out.println(strayNumber(numbers));
+//        System.out.println(populationGrowth(1000, 2, 50, 3200) + " years will be needed");
+
+
+    }
+
+    public static int populationGrowth (int p0, double percent, int aug, int p) {
+
+        int n = 0;
+        Double pDouble = Double.valueOf(p0);
+
+        while (p >= pDouble) {
+            pDouble = pDouble + pDouble*percent*0.01 + aug;
+            n++;
+
+            if (pDouble >= p) {
+                return n;
+            }
+        }
+
+        return 0;
+    }
+
+    public static int strayNumber (int[] numbers) {
+
+        int counter = 0;
+
+        for (int i=0; i<numbers.length; i++) {
+            if ((numbers[0] != numbers[i]) || (numbers[1] != numbers[i])) {
+                return numbers[i];
+            }
+
+        }
+        return counter;
     }
 
     public static String morseDecoder(String morseCode) {
